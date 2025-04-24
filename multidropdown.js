1e5ed01c03efd1565dac3de327712db9c5954085
Piotr Kalucki <piotr@lagrange.dev>
2025-04-24 13:06:50 +0200

    fix filtering and selects

--- multidropdown.js
+++ multidropdown.js
@@ -1,5 +1,5 @@
-var style = document.createElement("style");
-style.setAttribute("id", "multiselect_dropdown_styles");
+var style = document.createElement('style');
+style.setAttribute('id', 'multiselect_dropdown_styles');
 style.innerHTML = `
 .multiselect-dropdown{
   display: inline-block;
@@ -11,7 +11,7 @@ style.innerHTML = `
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
   background-repeat: no-repeat;
   background-position: right .75rem center;
-  background-size: 16px 12px;
+  background-size: 14px 12px;
 }
 .multiselect-dropdown span.optext, .multiselect-dropdown span.placeholder{
   margin-right:0.5em; 
@@ -88,60 +88,58 @@ document.head.appendChild(style);
 function MultiselectDropdown(options) {
   var config = {
     search: true,
-    height: "15rem",
-    placeholder: "select",
-    txtSelected: "selected",
-    txtAll: "All",
-    txtRemove: "Remove",
-    txtSearch: "search",
+    height: '15rem',
+    placeholder: 'select',
+    txtSelected: 'selected',
+    txtAll: 'All',
+    txtRemove: 'Remove',
+    txtSearch: 'search',
     ...options,
   };
   function newEl(tag, attrs) {
     var e = document.createElement(tag);
     if (attrs !== undefined)
       Object.keys(attrs).forEach((k) => {
-        if (k === "class") {
+        if (k === 'class') {
           Array.isArray(attrs[k])
-            ? attrs[k].forEach((o) => (o !== "" ? e.classList.add(o) : 0))
-            : attrs[k] !== ""
-            ? e.classList.add(attrs[k])
-            : 0;
-        } else if (k === "style") {
+            ? attrs[k].forEach((o) => (o !== '' ? e.classList.add(o) : 0))
+            : attrs[k] !== ''
+              ? e.classList.add(attrs[k])
+              : 0;
+        } else if (k === 'style') {
           Object.keys(attrs[k]).forEach((ks) => {
             e.style[ks] = attrs[k][ks];
           });
-        } else if (k === "text") {
-          attrs[k] === "" ? (e.innerHTML = "&nbsp;") : (e.innerText = attrs[k]);
+        } else if (k === 'text') {
+          attrs[k] === '' ? (e.innerHTML = '&nbsp;') : (e.innerText = attrs[k]);
         } else e[k] = attrs[k];
       });
     return e;
   }
 
-  document.querySelectorAll("select[multiple]").forEach((el, k) => {
-    var div = newEl("div", {
-      class: "multiselect-dropdown",
+  document.querySelectorAll('select[multiple]').forEach((el, k) => {
+    var div = newEl('div', {
+      class: 'multiselect-dropdown',
       style: {
-        width: config.style?.width ?? el.clientWidth + "px",
-        padding: config.style?.padding ?? "",
+        width: config.style?.width ?? el.clientWidth + 'px',
+        padding: config.style?.padding ?? '',
       },
     });
-    el.style.display = "none";
+    el.style.display = 'none';
     el.parentNode.insertBefore(div, el.nextSibling);
-    var listWrap = newEl("div", { class: "multiselect-dropdown-list-wrapper" });
-    var list = newEl("div", {
-      class: "multiselect-dropdown-list",
+    var listWrap = newEl('div', { class: 'multiselect-dropdown-list-wrapper' });
+    var list = newEl('div', {
+      class: 'multiselect-dropdown-list',
       style: { height: config.height },
     });
-    var search = newEl("input", {
-      class: ["multiselect-dropdown-search"].concat([
-        config.searchInput?.class ?? "form-control",
+    var search = newEl('input', {
+      class: ['multiselect-dropdown-search'].concat([
+        config.searchInput?.class ?? 'form-control',
       ]),
       style: {
-        width: "100%",
+        width: '100%',
         display:
-          el.attributes["multiselect-search"]?.value === "true"
-            ? "block"
-            : "none",
+          el.attributes['multiselect-search']?.value === 'true' ? 'block' : 'none',
       },
       placeholder: config.txtSearch,
     });
@@ -150,45 +148,40 @@ function MultiselectDropdown(options) {
     listWrap.appendChild(list);
 
     el.loadOptions = () => {
-      list.innerHTML = "";
+      list.innerHTML = '';
 
-      if (el.attributes["multiselect-select-all"]?.value == "true") {
-        var op = newEl("div", { class: "multiselect-dropdown-all-selector" });
-        var ic = newEl("input", { type: "checkbox" });
+      if (el.attributes['multiselect-select-all']?.value == 'true') {
+        var op = newEl('div', { class: 'multiselect-dropdown-all-selector' });
+        var ic = newEl('input', { type: 'checkbox' });
         op.appendChild(ic);
-        op.appendChild(newEl("label", { text: config.txtAll }));
+        op.appendChild(newEl('label', { text: config.txtAll }));
 
-        op.addEventListener("click", () => {
-          op.classList.toggle("checked");
-          op.querySelector("input").checked =
-            !op.querySelector("input").checked;
+        op.addEventListener('click', () => {
+          op.classList.toggle('checked');
+          op.querySelector('input').checked = !op.querySelector('input').checked;
 
-          var ch = op.querySelector("input").checked;
+          var ch = op.querySelector('input').checked;
           list
-            .querySelectorAll(
-              ":scope > div:not(.multiselect-dropdown-all-selector)"
-            )
+            .querySelectorAll(':scope > div:not(.multiselect-dropdown-all-selector)')
             .forEach((i) => {
-              if (i.style.display !== "none") {
-                i.querySelector("input").checked = ch;
+              if (i.style.display !== 'none') {
+                i.querySelector('input').checked = ch;
                 i.optEl.selected = ch;
               }
             });
 
-          el.dispatchEvent(new Event("change"));
+          el.dispatchEvent(new Event('change'));
         });
-        ic.addEventListener("click", (ev) => {
+        ic.addEventListener('click', (ev) => {
           ic.checked = !ic.checked;
         });
-        el.addEventListener("change", (ev) => {
+        el.addEventListener('change', (ev) => {
           let itms = Array.from(
             list.querySelectorAll(
-              ":scope > div:not(.multiselect-dropdown-all-selector)"
+              ':scope > div:not(.multiselect-dropdown-all-selector)'
             )
-          ).filter((e) => e.style.display !== "none");
-          let existsNotSelected = itms.find(
-            (i) => !i.querySelector("input").checked
-          );
+          ).filter((e) => e.style.display !== 'none');
+          let existsNotSelected = itms.find((i) => !i.querySelector('input').checked);
           if (ic.checked && existsNotSelected) ic.checked = false;
           else if (ic.checked == false && existsNotSelected === undefined)
             ic.checked = true;
@@ -198,19 +191,18 @@ function MultiselectDropdown(options) {
       }
 
       Array.from(el.options).map((o) => {
-        var op = newEl("div", { class: o.selected ? "checked" : "", optEl: o });
-        var ic = newEl("input", { type: "checkbox", checked: o.selected });
+        var op = newEl('div', { class: o.selected ? 'checked' : '', optEl: o });
+        var ic = newEl('input', { type: 'checkbox', checked: o.selected });
         op.appendChild(ic);
-        op.appendChild(newEl("label", { text: o.text }));
+        op.appendChild(newEl('label', { text: o.text }));
 
-        op.addEventListener("click", () => {
-          op.classList.toggle("checked");
-          op.querySelector("input").checked =
-            !op.querySelector("input").checked;
+        op.addEventListener('click', () => {
+          op.classList.toggle('checked');
+          op.querySelector('input').checked = !op.querySelector('input').checked;
           op.optEl.selected = !!!op.optEl.selected;
-          el.dispatchEvent(new Event("change"));
+          el.dispatchEvent(new Event('change'));
         });
-        ic.addEventListener("click", (ev) => {
+        ic.addEventListener('click', (ev) => {
           ic.checked = !ic.checked;
         });
         o.listitemEl = op;
@@ -220,33 +212,31 @@ function MultiselectDropdown(options) {
 
       div.refresh = () => {
         div
-          .querySelectorAll("span.optext, span.placeholder")
+          .querySelectorAll('span.optext, span.placeholder')
           .forEach((t) => div.removeChild(t));
         var sels = Array.from(el.selectedOptions);
-        if (
-          sels.length > (el.attributes["multiselect-max-items"]?.value ?? 5)
-        ) {
+        if (sels.length > (el.attributes['multiselect-max-items']?.value ?? 5)) {
           div.appendChild(
-            newEl("span", {
-              class: ["optext", "maxselected"],
-              text: sels.length + " " + config.txtSelected,
+            newEl('span', {
+              class: ['optext', 'maxselected'],
+              text: sels.length + ' ' + config.txtSelected,
             })
           );
         } else {
           sels.map((x) => {
-            var c = newEl("span", {
-              class: "optext",
+            var c = newEl('span', {
+              class: 'optext',
               text: x.text,
               srcOption: x,
             });
-            if (el.attributes["multiselect-hide-x"]?.value !== "true")
+            if (el.attributes['multiselect-hide-x']?.value !== 'true')
               c.appendChild(
-                newEl("span", {
-                  class: "optdel",
-                  text: "🗙",
+                newEl('span', {
+                  class: 'optdel',
+                  text: '🗙',
                   title: config.txtRemove,
                   onclick: (ev) => {
-                    c.srcOption.listitemEl.dispatchEvent(new Event("click"));
+                    c.srcOption.listitemEl.dispatchEvent(new Event('click'));
                     div.refresh();
                     ev.stopPropagation();
                   },
@@ -258,9 +248,9 @@ function MultiselectDropdown(options) {
         }
         if (0 == el.selectedOptions.length)
           div.appendChild(
-            newEl("span", {
-              class: "placeholder",
-              text: el.attributes["placeholder"]?.value ?? config.placeholder,
+            newEl('span', {
+              class: 'placeholder',
+              text: el.attributes['placeholder']?.value ?? config.placeholder,
             })
           );
       };
@@ -268,32 +258,30 @@ function MultiselectDropdown(options) {
     };
     el.loadOptions();
 
-    search.addEventListener("input", () => {
+    search.addEventListener('input', () => {
       list
-        .querySelectorAll(":scope div:not(.multiselect-dropdown-all-selector)")
+        .querySelectorAll(':scope div:not(.multiselect-dropdown-all-selector)')
         .forEach((d) => {
-          var txt = d.querySelector("label").innerText.toUpperCase();
-          d.style.display = txt.includes(search.value.toUpperCase())
-            ? "block"
-            : "none";
+          var txt = d.querySelector('label').innerText.toUpperCase();
+          d.style.display = txt.includes(search.value.toUpperCase()) ? 'block' : 'none';
         });
     });
 
-    div.addEventListener("click", () => {
-      div.listEl.style.display = "block";
+    div.addEventListener('click', () => {
+      div.listEl.style.display = 'block';
       search.focus();
       search.select();
     });
 
-    document.addEventListener("click", function (event) {
+    document.addEventListener('click', function (event) {
       if (!div.contains(event.target)) {
-        listWrap.style.display = "none";
+        listWrap.style.display = 'none';
         div.refresh();
       }
     });
   });
 }
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   MultiselectDropdown(window.MultiselectDropdownOptions);
 });
